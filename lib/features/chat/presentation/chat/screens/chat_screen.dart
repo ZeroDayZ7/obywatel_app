39df134353e011db91ac816755374fd282581189
ZeroDayZ7 <ZeroDayZ7@proton.me>
2025-11-19 16:07:10 +0100

    fix: imports

--- lib/features/chat/presentation/chat/screens/chat_screen.dart
+++ lib/features/chat/presentation/chat/screens/chat_screen.dart
@@ -1,9 +1,9 @@
 // lib/features/chat/presentation/chat/screens/chat_screen.dart
 import 'package:flutter/material.dart';
-import '../widgets/chat_screen/chat_app_bar.dart';
-import '../widgets/chat_screen/category_tabs.dart';
-import '../widgets/chat_screen/chat_list.dart';
-import '../widgets/chat_screen/bottom_nav_bar.dart';
+import 'package:obywatel_plus/features/chat/presentation/chat/widgets/chat_screen/chat_app_bar.dart';
+import 'package:obywatel_plus/features/chat/presentation/chat/widgets/chat_screen/category_tabs.dart';
+import 'package:obywatel_plus/features/chat/presentation/chat/widgets/chat_screen/chat_list.dart';
+import 'package:obywatel_plus/features/chat/presentation/chat/widgets/chat_screen/bottom_nav_bar.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
