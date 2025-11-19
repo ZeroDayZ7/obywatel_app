779badc1dca47fbef2fc6fa54fd6ee020fa7b39d
ZeroDayZ7 <ZeroDayZ7@proton.me>
2025-11-19 13:14:58 +0100

    chore: install base packages for secure chat implementation

---
+++ docs/command.md
@@ -0,0 +1,14 @@
+flutter --version # Pokazuje aktualną wersję Fluttera i Dart
+flutter doctor # Sprawdza środowisko Flutter/Dart, SDK, IDE, emulator
+flutter channel # Wyświetla dostępne kanały i aktywny (\*)
+flutter upgrade # Aktualizuje Fluttera do najnowszej wersji w aktywnym kanale
+flutter clean # Czyści build (usuwa foldery build, .dart_tool)
+flutter pub get # Pobiera zależności z pubspec.yaml
+flutter pub upgrade # Aktualizuje zależności
+flutter pub outdated # Sprawdza, które paczki mają nowsze wersje
+flutter pub cache repair # Naprawia cache paczek
+flutter analyze # Analizuje kod pod kątem błędów i ostrzeżeń
+flutter channel <channel> # Zmienia kanał: stable, beta, dev, master
+flutter upgrade # Po zmianie kanału aktualizuje Fluttera
+flutter format . # Formatuje cały kod w projekcie
+flutter devices # Lista podłączonych urządzeń/emulatorów
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,17 +6,23 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
+import file_picker
+import file_selector_macos
 import flutter_secure_storage_macos
 import local_auth_darwin
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
+import video_compress
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  VideoCompressPlugin.register(with: registry.registrar(forPlugin: "VideoCompressPlugin"))
 }
