8478e3727ad9bb61b349846f409bc7ecc6c5fb12
ZeroDayZ7 <ZeroDayZ7@proton.me>
2026-08-04 17:03:39 +0200

    fix: comment out unused logger and observer imports in router configuration

--- lib/app/router/router_config.dart
+++ lib/app/router/router_config.dart
@@ -6,7 +6,7 @@ import 'package:obywatel_plus/app/router/app_routes.dart';
 import 'package:obywatel_plus/app/router/redirect/redirect_logic.dart';
 import 'package:obywatel_plus/core/errors/presentation/error_screen.dart';
 import 'package:obywatel_plus/core/logger/logger_provider.dart';
-import 'package:obywatel_plus/core/logger/observers/app_router_observer.dart';
+// import 'package:obywatel_plus/core/logger/observers/app_router_observer.dart';
 import 'package:obywatel_plus/features/auth/application/auth/auth_refresh_provider.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
@@ -14,13 +14,13 @@ GlobalKey<NavigatorState> get rootNavigatorKey => _rootNavigatorKey;
 
 GoRouter createRouter({required Ref ref, required List<RouteBase> routes}) {
   final refreshListenable = ref.watch(authRefreshListenableProvider.notifier);
-  final logger = ref.read(appLoggerProvider);
+  // final logger = ref.read(appLoggerProvider);
 
   return GoRouter(
     navigatorKey: _rootNavigatorKey,
     initialLocation: AppRoutes.initial,
     refreshListenable: refreshListenable,
-    observers: [AppRouterObserver(logger)],
+    // observers: [AppRouterObserver(logger)],
     routes: routes,
     redirect: (context, state) => appRedirectLogic(ref, state),
     errorBuilder: (context, state) {
